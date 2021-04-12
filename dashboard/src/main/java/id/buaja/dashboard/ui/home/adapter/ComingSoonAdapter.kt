2701d94c4bacc588456077af0e519f99f23fc834
egifcb <egifcb@gmail.com>
2021-04-13 00:02:50 +0700

    fixing coming soon

--- dashboard/src/main/java/id/buaja/dashboard/ui/home/adapter/ComingSoonAdapter.kt
+++ dashboard/src/main/java/id/buaja/dashboard/ui/home/adapter/ComingSoonAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import id.buaja.dashboard.databinding.ItemListPopularBinding
+import id.buaja.dashboard.databinding.ItemListPopularHomeBinding
 import id.buaja.domain.model.ComingSoon
 
 /**
@@ -17,7 +18,7 @@ class ComingSoonAdapter(private val data: List<ComingSoon>, private val listener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(
-            ItemListPopularBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ItemListPopularHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
 
     override fun getItemCount() = if (data.size > 10) {
@@ -29,7 +30,7 @@ class ComingSoonAdapter(private val data: List<ComingSoon>, private val listener
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
         holder.bind(data[position], listener)
 
-    class ViewHolder(private val binding: ItemListPopularBinding) :
+    class ViewHolder(private val binding: ItemListPopularHomeBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ComingSoon, listener: (ComingSoon) -> Unit) {
             with(binding) {
